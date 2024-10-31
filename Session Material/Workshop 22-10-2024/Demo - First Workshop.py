e34f8d9b4c11b26ea0188410b7f82c37ba5de174
abdallahsoliman00 <abdallahsoliman03@gmail.com>
2024-10-31 13:12:27 +0000

    Made Demo file user friendly and uploaded documentation

--- Session Material/Workshop 22-10-2024/Demo - First Workshop.py
+++ Session Material/Workshop 22-10-2024/Demo - First Workshop.py
@@ -3,11 +3,7 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 
 
-def get_stock_data(symbol, start_date, end_date):
-    stock_data = yf.download(symbol, start=start_date, end=end_date)
-    return stock_data
-
-
+# Functions used to import data from Yahoo Finance
 def get_data(ticker, start_date, end_date):
     data = yf.download(ticker, start_date, end_date)
     return data
@@ -23,35 +19,35 @@ def plot_symbol(symbol, start, end, color='black'):
     plt.show()
 
 
-# Plot stock data
-# plot_symbol("^GSPC", '2000-01-28', '2024-01-26', 'r')
+# Plot S&P 500 market data
+plot_symbol("^GSPC", '2000-01-28', '2024-01-26', 'r')
 
+# Predefining parameters
 growthRate = 0.08
 pi = np.pi
-
 t = np.linspace(1, 100, 6036)
 
-# Evolution
-# x = np.exp(growthRate*t)
+# Model Evolution
+# To use these, remove the comment from the line you want to run, while commenting the rest, and run the program
 
-# x = np.exp(growthRate*t) + np.sin(2*pi*1*t)
+x = np.exp(growthRate*t)
 
-# x = np.exp(growthRate*t) + (np.exp(growthRate*t)*(np.sin(2*pi*1*t)))/2
-# plt.plot(t, np.exp(growthRate*t), linestyle='--', color='k')
+# x = np.exp(growthRate*t) + 5*np.sin(2*pi*1*t)
 
-# x = 1000+ np.exp(growthRate*t) + 0.2*np.exp(growthRate*t)*(np.sin(2*pi*0.25*t))
-# plt.plot(t, 0.5*np.exp(growthRate*t), linestyle='--', color='k')
-# plt.plot(t, 1.5*np.exp(growthRate*t), linestyle='--', color='k')
+# x = np.exp(growthRate*t) + 0.4*np.exp(growthRate*t)*(np.sin(2*pi*0.25*t))
 
-# x = np.exp(growthRate*t) + np.exp(growthRate*t)*( 0.035*np.sin(2*pi*4*t) + 0.1*np.sin(2*pi*0.25*t) )
+# x = 1000 + np.exp(growthRate*t)*(1 + 0.1*np.sin(2*pi*1*t) + 0.2*np.sin(2*pi*0.25*t) + 0.08*np.sin(2*pi*0.35*t))
 
-x = 1000 + np.exp(growthRate*t) + 0.1*np.exp(growthRate*t)*(np.sin(2*pi*1*t) + np.sin(2*pi*0.25*t) + np.sin(2*pi*0.35*t))
 
+# Plot evolution against market data
 plt.plot(t, get_data("^GSPC", '2000-01-28', '2024-01-26')['Adj Close'], color='r', linewidth=0.7)
 plt.plot(t, x, color='green', linewidth=0.7)
 plt.show()
 
 # Plot market cycles
-# plt.plot(t, (np.sin(2*pi*1*t) + np.sin(2*pi*0.25*t) + np.sin(2*pi*0.35*t)))
-plt.plot(t, ( 0.035*np.sin(2*pi*5*t) + 0.1*np.sin(2*pi*0.25*t) + 0.15*np.sin(2*pi*0.4*t) ))
+t = t[0:int(len(t)/4)]
+cycles = 0.1*np.sin(2*pi*1*t) + 0.2*np.sin(2*pi*0.25*t) + 0.08*np.sin(2*pi*0.35*t)
+plt.figure(figsize=(9,5))
+plt.plot(t, cycles)
+plt.title("Market Cycles")
 plt.show()
